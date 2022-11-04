de7ab33a7fd88c7f288bcb3b6115031287ac3a50
adden <a.d.denusiv@mail.ru>
2022-11-04 18:19:27 +0600

    updated splash activity

--- app/src/main/java/com/example/test2effectivemobile/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/test2effectivemobile/presentation/splash/SplashActivity.kt
@@ -1,13 +1,39 @@
 package com.example.test2effectivemobile.presentation.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.test2effectivemobile.R
+import android.view.Window
+import android.view.WindowManager
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.test2effectivemobile.databinding.ActivitySplashBinding
+import com.example.test2effectivemobile.presentation.homestore.HomeStoreActivity
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
-
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        viewModel.isLoaded.observe(
+            this
+        ) {
+            if (it) {
+                startActivity(Intent(this, HomeStoreActivity::class.java))
+                finish()
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/test2effectivemobile/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/test2effectivemobile/presentation/splash/SplashViewModel.kt
@@ -1,9 +1,25 @@
 package com.example.test2effectivemobile.presentation.splash
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SplashViewModel: ViewModel() {
+@HiltViewModel
+class SplashViewModel @Inject constructor() : ViewModel() {
+    val isLoaded = MutableLiveData<Boolean>()
 
+    init {
+        isLoaded.value = false
+        viewModelScope.launch {
+            delay(timeMillis = 2000)
+            isLoaded.postValue(true)
+
+        }
+    }
 
 
 }
